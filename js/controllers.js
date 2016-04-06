4b1d8acf7c7e46b32dccb620f89fe211619243c2
Davis Gossage <dcg13@duke.edu>
2016-04-05 20:32:40 -0400

    Switching to poker, add state logic

--- js/controllers.js
+++ js/controllers.js
@@ -5,35 +5,46 @@ $(function() {
     FastClick.attach(document.body);
 });
 
-gameApp.controller('GameCenter', ['$scope', 'parseCenter', 'pusherCenter', function ($scope, $parseCenter, $pusherCenter){
+gameApp.controller('GameCenter', ['$scope', 'parseCenter', 'pubnubCenter', function ($scope, $parseCenter, $pubnubCenter){
 	$scope.gameIdInput = '';
 	$scope.nameInput = '';
 
-	$scope.joiningGame = false;
+	GAME_STATUS = {
+		PREJOIN : 0,
+		JOINING : 1,
+		CONNECTING : 2,
+		INGAME : 3,
+		WAITING : 4,
+		ERRORED : 5
+	}
 
-	$scope.gameJoinScreenShown = true;
-	$scope.playerWaitScreenShown = false;
+	$scope.status = GAME_STATUS.PREJOIN
 
-	$scope.numPlayers = 0;
+	$scope.chips = 0;
+
+	//annoying angular scope ng-show variables
+	$scope.joinScreenShown = function(){return $scope.status == GAME_STATUS.PREJOIN || $scope.status == GAME_STATUS.JOINING};
+	$scope.joiningGame = function(){return $scope.status == GAME_STATUS.JOINING};
+	$scope.findingSeat = function(){return $scope.status == GAME_STATUS.CONNECTING};
+	$scope.waitingForSeat = function(){return $scope.status == GAME_STATUS.WAITING};
+	$scope.inGame = function(){return $scope.status ==  GAME_STATUS.INGAME};
+
 
 	$scope.joinGame = function() {
 		if ($scope.gameIdInput.length > 0 && $scope.nameInput.length > 0){
 			var gameObj;
 			
-			$scope.joiningGame = true;
-
+			$scope.status = GAME_STATUS.JOINING
 			$parseCenter.locateGame($scope.gameIdInput).then(function(gameO){
 				gameObj = gameO;
 				return $parseCenter.addPlayerToGame($scope.nameInput, gameObj);
 			}).then(function(playerObj){
-				$scope.gameJoinScreenShown = false;
-				$scope.playerWaitScreenShown = true;
-				$pusherCenter.subscribeToChannel(gameObj.get("channelName"), playerObj.get("name"), playerObj.id);
-				$pusherCenter.bindToPlayerConnectionEvent(playerAdded);
+				$scope.status = GAME_STATUS.CONNECTING
+				$pubnubCenter.subscribeToChannel(gameObj.get("channelName"), playerObj.get("name"), playerObj.id, stateChanged);
 				$scope.$apply();
 			}, function(error){
 				alert(error.message);
-				$scope.joiningGame = false;
+				$scope.status = GAME_STATUS.ERRORED;
 			});
 		}
 		else if($scope.gameIdInput.length == 0){
@@ -48,9 +59,19 @@ gameApp.controller('GameCenter', ['$scope', 'parseCenter', 'pusherCenter', funct
 		console.log(direction);
 	}
 
-	playerAdded = function(player){
-		$scope.numPlayers = $pusherCenter.playersConnected();
-		$scope.$apply();
+	stateChanged = function(m){
+		if (m.action == "state-change"){
+			//is this a game status change?
+			console.log(m);
+			if (m.data && m.data.GAME_STATUS){
+				$scope.status = GAME_STATUS[m.data.GAME_STATUS]
+				$scope.$apply()
+			}
+			if (m.data && m.data.CHIPS){
+				$scope.chips = m.data.CHIPS
+				$scope.$apply()
+			}
+		}
 	}
 
 }]);
@@ -78,38 +99,55 @@ gameApp.factory('parseCenter', ['$q', function ($q) {
 	return service;
 }]);
 
-gameApp.factory('pusherCenter', [function () {
+gameApp.factory('pubnubCenter', [function () {
 	var service = {};
 
-	var pusher;
+	var pubnub;
 	var channel;
 	var playerId;
 
-	service.subscribeToChannel = function(channelId, name, userId){
+	service.subscribeToChannel = function(channelId, name, userId, callback){
 		playerId = userId;
-		pusher = new Pusher('22228a560a162feb172a', {
-			authEndpoint: 'https://battle-tanks.parseapp.com/pusher/auth',
-			auth: {
-				params: {
-					user_name: name,
-					user_id: userId
-				}
-			}
+		channel = channelId;
+		pubnub = PUBNUB({
+			publish_key: "pub-c-9629b438-9328-4fb1-9cb5-156752315d42",
+			subscribe_key: "sub-c-d0635f8a-fa00-11e5-8916-0619f8945a4f",
+			uuid: userId
 		});
-		channel = pusher.subscribe(channelId);
+		pubnub.subscribe({
+			channel: channel,
+			state:{
+				username: name
+			},
+			message : function (message, envelope, channelOrGroup, time, channel) {
+        console.log(
+        "Message Received." + "\n" +
+        "Channel or Group: " + JSON.stringify(channelOrGroup) + "\n" +
+        "Channel: " + JSON.stringify(channel) + "\n" +
+        "Message: " + JSON.stringify(message) + "\n" +
+        "Time: " + time + "\n" +
+        "Raw Envelope: " + JSON.stringify(envelope)
+      )},
+			presence: callback,
+			connect: join
+		});
+	}
+
+	function join() {
+		console.log('joined');
 	}
 
 	service.playersConnected = function(){
 		return channel.members.count;
 	}
 
-	service.bindToPlayerConnectionEvent = function(receivingFunction){
-		channel.bind('pusher:member_added', receivingFunction);
-		channel.bind('pusher:subscription_succeeded', receivingFunction);
-	}
-
 	service.publishMovementEvent = function(direction){
-		channel.trigger('client-movement-event', {message : direction, client: playerId})
+		pubnub.state({
+			channel: channel,
+			state:{
+				direction: direction
+			}
+		});
 	}
 
 	return service;
--- js/directives.js
+++ js/directives.js
@@ -1,9 +1,23 @@
-gameApp.directive('gamepad', ['$interval', 'pusherCenter', function ($interval, $pusherCenter) {
+gameApp.directive('gamepad', ['$interval', 'pubnubCenter', function ($interval, $pubnubCenter) {
    return {
       templateUrl: 'gamepad.html',
       restrict: 'E',
       link: function (scope, element, attrs) {
-        element.addClass('gamepad');
+        element[0].children[0].innerText = "Move "+element[0].id
+
+        element.on('mousedown', function(event){
+          $pubnubCenter.publishMovementEvent(element[0].id.toLowerCase());
+          setTimeout(function(){
+            $pubnubCenter.publishMovementEvent(element[0].id.toLowerCase());
+          }, 12)
+        });
+
+        element.on('mouseup', function(event){
+          $pubnubCenter.publishMovementEvent("");
+          setTimeout(function(){
+            $pubnubCenter.publishMovementEvent("");
+          }, 12)
+        })
 
         element.on('touchstart', function(event){
           console.log(attrs);
@@ -11,7 +25,7 @@ gameApp.directive('gamepad', ['$interval', 'pusherCenter', function ($interval, 
 
         element.on('touchend', function(event){
           console.log(element);
-        })
+        });
     }
   };
 }]);
